55c3e2cb16438e64635b9d4fbb5c186e60b904b0
Anders Pitman <tapitman11@gmail.com>
2022-05-11 23:39:52 -0600

    Add CNAME to build

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   return {
   }
